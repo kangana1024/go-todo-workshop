255499d2b80a27fa1d9a2754fdb5cb4c254c5ecc
Kanchana Sondee <kangana512@gmail.com>
2021-06-20 21:50:42 +0700

    create todo

--- controllers/todo.go
+++ controllers/todo.go
@@ -1,8 +1,38 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"database/sql"
+	"fmt"
+	"gotodo/database"
+	"gotodo/models"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type CreateRequest struct {
+	Todo   string `json:"todo"`
+	Detail string `json:"detail"`
+}
 
 func Create(c *fiber.Ctx) error {
+	var req CreateRequest
+	if err := c.BodyParser(&req); err != nil {
+		return err
+	}
 
-	return nil
+	var todo models.Todo
+	todo.Todo = req.Todo
+	if req.Detail != "" {
+		todo.Detail = sql.NullString{String: req.Detail, Valid: true}
+	}
+	todo.Status = "p"
+
+	result := database.DB.Create(&todo)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return c.JSON(fiber.Map{
+		"id": fmt.Sprintf("%d", todo.ID),
+	})
 }
